db76f7e8703f2ed4aa788d5c4b245b0ca01cde60
origami <1071242996@qq.com>
2023-04-05 13:31:53 +0800

    完成banner组件

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,8 +49,11 @@ export const NavBar = () => {
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
+                            {/*eslint-disable-next-line */}
                             <a href="#"><img src={navIcon1} alt=""/></a>
+                            {/*eslint-disable-next-line */}
                             <a href="#"><img src={navIcon2} alt=""/></a>
+                            {/*eslint-disable-next-line */}
                             <a href="#"><img src={navIcon3} alt=""/></a>
                         </div>
                         <button className="vvd" onClick={() => {
