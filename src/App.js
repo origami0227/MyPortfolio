969794fbd9467aba4e5e27e94efe16d25eec2f79
origami <1071242996@qq.com>
2023-04-12 09:29:45 +0800

    feature：完成Footer组件，基本完成个人作品集

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {NavBar} from "./components/NavBar";
 import {Banner} from "./components/Banner";
 import {Skills} from "./components/Skills";
 import {Projects} from "./components/Projects";
+import {Footer} from "./components/Footer";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Banner/>
             <Skills/>
             <Projects/>
+            <Footer/>
         </div>
     );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,26 @@
+import {Container, Row, Col} from "react-bootstrap";
+import logo from "../assets/img/logo.svg"
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
 export const Footer = () => {
     return (
-        <section></section>
+        <footer className="footer">
+            <Container>
+                <Row className="align-items-center">
+                    <Col sm={6}>
+                        <img src={logo} alt=""/>
+                    </Col>
+                    <Col sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                            <a href=""><img src={navIcon1} alt=""/></a>
+                            <a href=""><img src={navIcon2} alt=""/></a>
+                            <a href=""><img src={navIcon3} alt=""/></a>
+                            <p>CopyRight 2022. All right</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
